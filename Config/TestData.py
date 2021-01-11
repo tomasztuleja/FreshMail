26067f403010af225fedf4bb95cc7e08a44599d3
tomasztuleja <tuleja.tomasz@gmail.com>
2021-01-11 23:08:04 +0100

    2nd commit

---
+++ Config/TestData.py
@@ -0,0 +1,9 @@
+class TestData:
+
+    CHROME_EXECUTABLE_PATH = "D:/DevTools/chromeDriver/chromedriver.exe"
+    BASE_URL = "https://app.freshmail.com/pl/auth/login/"
+    LOGIN = "yogy@onet.eu"
+    PASSWORD = "Tomek1987"
+
+
+
